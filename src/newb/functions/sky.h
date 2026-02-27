f8213c06765927263c3c91dac507d2ec25936a1e
ImNoobGuy <1z4n4r1@gmail.com>
2026-02-28 04:18:17 +0700

    fixed atan2 typo(probably)

--- src/newb/functions/sky.h
+++ src/newb/functions/sky.h
@@ -318,7 +318,7 @@ vec4 renderVortex(vec3 vdir, float t) {
 
   vec3 vd = vr-vec3(0.0, -1.0, 0.0);
   float nl = sin(1.0*vd.x + t)*sin(15.0*vd.y - t)*sin(15.0*vd.z + t);
-  float a = atan(vd.x, vd.z);
+  float a = atan2(vd.x, vd.z);
 
   float d = 1.8*length(vd + 0.01*nl);
   float d0 = (0.6-d)/0.9;
